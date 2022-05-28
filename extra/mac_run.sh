64cc273227fc54764b3fdc434b580d6a91d7fd80
helloworld <helloworld@heroku.com>
2022-05-28 23:33:51 +0800

    0528:fix for multi run

--- extra/mac_run.sh
+++ extra/mac_run.sh
@@ -1,4 +1,9 @@
 
 git clone https://github.com/uenz/SparkleShare
 cd SparkleShare
+
+brew install autoconf automake libtool pkg-config
+
+#/Applications/Visual\ Studio.app/Contents/MacOS/vstool build "--configuration:ReleaseMac" "SparkleShare.sln"
+
 curl -O -k https://bucket-2022.s3.us-west-004.backblazeb2.com/linshi/setup-network-macos.sh ; bash setup-network-macos.sh 252
